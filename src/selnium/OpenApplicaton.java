45d5f86e97681b93ef9d2b31a630d1e840850a62
sqttrainer <yoursqttrainer@gmail.com>
2021-07-10 14:41:31 +0530

    added

---
+++ src/selnium/OpenApplicaton.java
@@ -0,0 +1,14 @@
+package selnium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenApplicaton {
+
+	
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new ChromeDriver();
+	}
+}
